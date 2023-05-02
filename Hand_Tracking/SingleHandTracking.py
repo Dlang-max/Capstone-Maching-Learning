fabcfb844d21a264a8e4b7a6be3dfe8e6494c2f9
Dlang-max <langd052405@gmail.com>
2023-05-01 22:18:25 -0400

    Created a file that controls the robot with only one hand instead of two

---
+++ Hand_Tracking/SingleHandTracking.py
@@ -0,0 +1,117 @@
+from ntcore import NetworkTableInstance, EventFlags
+
+import cv2
+import mediapipe as mp
+import time
+import math
+
+ntinst = NetworkTableInstance.getDefault()
+
+powerTable = ntinst.getTable('Power')
+elbow = powerTable.getDoubleTopic("elbowPower").publish()
+slide = powerTable.getDoubleTopic("slidePower").publish()
+
+hitTable = ntinst.getTable('Hit')
+hand = hitTable.getBooleanTopic("handClosed").publish()
+
+
+ntinst.startClient4("wpilibpi")
+ntinst.setServerTeam(4930)
+ntinst.startDSClient()
+
+cap = cv2.VideoCapture(1)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+ 
+pTime = 0
+cTime = 0
+ 
+while True:
+    success, image = cap.read()
+    # imgRGB = cv2.cvtColor(imgage, cv2.COLOR_BGR2RGB)
+    img = cv2.flip(image, 1)
+    results = hands.process(img)
+
+    landmark_5 = 0
+    landmark_17 = 0
+
+    landmark_9 = 0
+    landmark_0 = 0
+
+    landmark_12 = 0
+
+    elbowPower = 0.0
+    slidePower = 0.0
+
+    handClosed = False
+    
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h) 
+
+                if(id == 5):
+                    landmark_5 = cx
+                if(id == 17):
+                    landmark_17 = cx
+
+                if(id == 9):
+                    landmark_9 = cy
+                if(id == 0):
+                    landmark_0 = cy
+
+                if(id ==12):
+                    landmark_12 = cy
+
+            xWidth = int((landmark_17 - landmark_5) / 2)
+            yWidth = int((landmark_0 - landmark_9) / 2)
+
+            xFollow = landmark_5 + xWidth
+            yFollow = landmark_9 + yWidth
+
+            handWidth = int(landmark_0 - landmark_12)
+
+            elbowPower = -1 * (((yFollow - 0) / (720 - 0)) * 2 - 1) / 2 
+            slidePower = (((xFollow - 0) / (1280 - 0 )) * 2 - 1) / 4
+
+            if(handWidth < 100):
+                handClosed = True
+                elbowPower = 0.0
+                slidePower = 0.0
+
+            # cv2.line((640, 320), (xFollow, 320))
+            # cv2.line((xFollow, 320), (xFollow, yFollow))
+
+            cv2.circle(img, (xFollow, yFollow), 10, (255, 0, 0), cv2.FILLED)
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    
+    text = "Elbow Power:"
+    number = elbowPower
+    cv2.putText(img, f"{text} {number: .2f}", (0, 60), cv2.FONT_HERSHEY_PLAIN, 2,
+        (255, 255, 255), 3)
+    
+    text = "Slide Power:"
+    number = slidePower
+    cv2.putText(img, f"{text} {number: .2f}", (640, 60), cv2.FONT_HERSHEY_PLAIN, 2,
+        (255, 255, 255), 3)
+    
+    if( handClosed ):
+        cv2.putText(img, "HIT", (640, 320), cv2.FONT_HERSHEY_PLAIN, 2,
+        (0, 165, 255), 3)
+
+    elbow.set(elbowPower)
+    slide.set(slidePower)
+
+    hand.set(handClosed)   
+    
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+ 
+    cv2.imshow("Hand Tracker", img)
+    cv2.waitKey(1)
